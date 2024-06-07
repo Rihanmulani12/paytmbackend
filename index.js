679df61602355b0583fb6332087475810427bfc6
Rihanmulani12 <rihanmulani18@gmail.com>
2024-06-07 12:46:02 +0530

    port fixed

--- index.js
+++ index.js
@@ -5,17 +5,17 @@ const cors = require('cors')
 require('dotenv').config();
 
 const app = express();
+const port = process.env.PORT || 4000;
 
 app.use(cors())
 app.use(bodyParser.json()); 
 
 app.use('/api', apiRouter); 
 
-const PORT =  process.env.PORT  || 3000;
 app.get("/" , (req, res)=>{
     res.send("hii this is root api")
 })
-app.listen(PORT, () => {
-    console.log(`Server is running on port ${PORT}`);
+app.listen(port, () => {
+    console.log(`Server is running on port ${port}`);
 });
 
